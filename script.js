6cc096cc9354c016a5256056bbb4baeff72fa9df
Niroojen Thambimuthu <niroojen1996@gmail.com>
2022-07-30 20:40:12 -0600

    Update
    Dealt with empty input

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function addItemToList(){
-    console.log("ITEM should be added!");
+    // console.log("ITEM should be added!");
 
     let inputText = document.getElementById("itemForList").value;
     let tempList = document.createElement("li");
-    tempList.innerText = inputText;
-    console.log(inputText);
-    console.log(tempList);
-    document.getElementById("listToDo").appendChild(tempList);
+    let addToList = document.getElementById("listToDo");
+
+    // item cannot be empty
+    if(inputText !== ""){
+        tempList.innerText = inputText;
+        addToList.appendChild(tempList);
+    }
+
+    // tempList.innerText = inputText;
+    // console.log(inputText);
+    // console.log(tempList);
+    
+
+    
 }
