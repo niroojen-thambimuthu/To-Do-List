3448f05cea4bd61054e11651b626914e3d0a1bb3
Niroojen Thambimuthu <niroojen1996@gmail.com>
2022-07-30 20:28:10 -0600

    Input text update
    Input text can enter ordered list

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function addItemToList(){
     console.log("ITEM should be added!");
+
+    let inputText = document.getElementById("itemForList").value;
+    let tempList = document.createElement("li");
+    tempList.innerText = inputText;
+    console.log(inputText);
+    console.log(tempList);
+    document.getElementById("listToDo").appendChild(tempList);
 }
